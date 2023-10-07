6618d9cc03361f063310f82428346774e1716f55
Peter Rubenstein <peter216@gmail.com>
2023-10-07 19:35:08 +0000

    added main protection

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 all_wins = {
     ("rock", "scissors"): "crushes",
     ("paper", "rock"): "covers",
@@ -14,7 +15,8 @@ def result_string(player_a, player_b):
         verb = all_wins[(player_b, player_a)]
         return f"{player_b} {verb} {player_a}! player B wins."
 
-choices = ["rock", "paper", "scissors"]
-for aaa in choices:
-    for bbb in choices:
-        print(result_string(aaa, bbb))
+if __name__ == '__main__':
+    choices = ["rock", "paper", "scissors"]
+    for aaa in choices:
+        for bbb in choices:
+            print(result_string(aaa, bbb))
