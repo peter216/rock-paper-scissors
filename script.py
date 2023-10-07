5256c449e28b1906ab99cf691ae4466c02226b92
Peter Rubenstein <peter216@gmail.com>
2023-10-07 19:18:50 +0000

    added script

---
+++ script.py
@@ -0,0 +1,20 @@
+all_wins = {
+    ("rock", "scissors"): "crushes",
+    ("paper", "rock"): "covers",
+    ("scissors", "paper"): "cuts"
+}
+
+def result_string(player_a, player_b):
+    if player_a == player_b:
+        return f"Both did {player_a}. They tied."
+    elif (player_a, player_b) in all_wins:
+        verb = all_wins[(player_a, player_b)]
+        return f"{player_a} {verb} {player_b}! player A wins."
+    else:
+        verb = all_wins[(player_b, player_a)]
+        return f"{player_b} {verb} {player_a}! player B wins."
+
+choices = ["rock", "paper", "scissors"]
+for aaa in choices:
+    for bbb in choices:
+        print(result_string(aaa, bbb))
