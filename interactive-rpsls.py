f8fff04b8bade872a5803b2a25a7143d95179d29
Peter Rubenstein <peter.rubenstein@marriott-sp.com>
2023-11-06 09:59:05 -0500

    Slight modifications

--- interactive-rpsls.py
+++ interactive-rpsls.py
@@ -22,11 +22,11 @@ class RPS():
         return self.entry.capitalize()
     def fights(self, other):
         if verb := RPS.all_wins.get((self.entry, other.entry)):
-            return f"{self.cap()} {verb} {other}! You win!"
+            return (0, f"{self.cap()} {verb} {other}! You win!")
         elif verb := RPS.all_wins.get((other.entry, self.entry)):
-            return f"{other.cap()} {verb} {self}! Computer wins!"
+            return (1, f"{other.cap()} {verb} {self}! Computer wins!")
         else:
-            return f"You tied."
+            return (2, f"You tied.")
 
 
 if __name__ == '__main__':
@@ -35,16 +35,26 @@ if __name__ == '__main__':
                "s": "scissors",
                "l": "lizard",
                "k": "Spock"}
+    score = [0, 0, 0]
     while True:
         print()
         aaa = input("(r)ock/(p)aper/(s)cissors/(l)izard/Spoc(k)/(q)uit? ").lower().strip()
         fchar = aaa[0] if len(aaa) else "0"
         if fchar == 'q':
+            wins, losses, ties = score
+            if wins >= losses:
+                print("Good game.")
+            else:
+                print("Better luck next time.")
             break
         elif aaa := choices.get(fchar):
             bbb = random.choice(list(choices.values()))
             print(f"You chose {aaa}.")
             print(f"Computer did {bbb}.")
-            print(RPS(aaa).fights(RPS(bbb)))
+            augment_index, verbiage = RPS(aaa).fights(RPS(bbb))
+            print(verbiage)
+            score[augment_index] += 1
+            wins, losses, ties = score
+            print(f"Score: {wins} wins, {losses} losses, {ties} ties")
         else:
             print("Invalid choice. Try again.")
